aae836e3819c013ac9303d519250142d195ef20d
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-08 16:49:41 -0300

    criando o service e a regra do login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { KeyboardAvoidingView, Platform, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 import logo from '../assets/logo.png';
+import api from '../services/api';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default function Login({ navigation }) {
-    function handleLogin() {
+    const [user, setUser] = useState('');
 
-        navigation.navigate('Main');
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user) {
+                navigation.navigate('Main', { user })
+            }
+        })
+    }, []);
+
+    async function handleLogin() {
+        const response = await api.post('/devs', { username: user });
+        const { _id } = response.data;
+        await AsyncStorage.setItem('user', _id);
+
+        navigation.navigate('Main', { _id });
     }
 
     return (
         <KeyboardAvoidingView
-        behavior="padding"
-        enabled={Platform.OS == 'ios'}
+            behavior="padding"
+            enabled={Platform.OS == 'ios'}
             style={styles.container}
         >
             <Image src={logo} />
@@ -21,6 +36,8 @@ export default function Login({ navigation }) {
                 placeholder="Digite seu usuário no Github"
                 placeholderTextColor="#999"
                 style={styles.input}
+                value={user}
+                onChangeText={setUser}
             />
             <TouchableOpacity onPress={handleLogin} style={styles.button}>
                 <Text style={styles.buttonText}>Enviar</Text>
